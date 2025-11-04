062c04a6b5a99cdcf58a7628c4392cc999c3f325
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 00:30:39 +0200

    Create GameObject class for engine
    An abstract template for all game objects

---
+++ Engine/GameObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Engine
+{
+    public abstract class GameObject
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; }
+        public bool Active { get; set; } = true;
+        private readonly List<Component> _components = new();
+
+        protected GameObject(string name) 
+        {
+            Name = name;
+        }
+
+        public T? GetComponent<T>() where T : Component =>
+            _components.OfType<T>().FirstOrDefault();
+
+        public void AddComponent(Component component)
+        {
+            component.Owner = this;
+            _components.Add(component);
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            foreach (var c in _components)
+                c.Update(deltaTime);
+        }
+
+        public override string ToString() => Name;
+    }
+
+    public abstract class Component
+    {
+        public GameObject Owner { get; internal set; }
+        public virtual void Update(float deltaTime) { }
+    }
+}
