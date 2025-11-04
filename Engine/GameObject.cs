3129b6d50a7f1e35ffe584c9c46176d9ae11db05
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 00:56:38 +0200

    Revert "Update engine classes namespaces"
    This reverts commit ddf6aef88bdfb7602a20166b147fb458429c5d68.

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvadedGame.Engine.GameObject
+namespace InvadedGame.Engine
 {
     public abstract class GameObject
     {
