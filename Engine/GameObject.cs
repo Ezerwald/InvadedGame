ddf6aef88bdfb7602a20166b147fb458429c5d68
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 00:45:33 +0200

    Update engine classes namespaces

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvadedGame.Engine
+namespace InvadedGame.Engine.GameObject
 {
     public abstract class GameObject
     {
