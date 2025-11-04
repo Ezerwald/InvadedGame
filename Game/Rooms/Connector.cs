f128852f90548c75dc808c452d1c886f45769999
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 01:02:43 +0200

    Created Connector class
    Connectors - game objects responsible for connecting rooms

---
+++ Game/Rooms/Connector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InvadedGame.Engine;
+
+namespace InvadedGame.Game.Rooms
+{
+    public enum ConnectorType
+    {
+        Corridor,
+        Vent
+    }
+
+    public class Connector : GameObject
+    {
+        public Room RoomA { get; }
+        public Room RoomB { get; }
+        public ConnectorType Type { get; }
+        public bool IsOpen { get; set; } = true;
+
+
+        public Connector(Room a, Room b, ConnectorType type, string? name = null)
+            : base(name ?? $"{type} {a.Name}-{b.Name}")
+        {
+            RoomA = a;
+            RoomB = b;
+            Type = type;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+        }
+
+        public override string ToString() =>
+            $"{Name}: {Type}, {(IsOpen ? "Open" : "Closed")}";
+    }
+}
