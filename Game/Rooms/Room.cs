7841390867eca4d48f00e5d08aa535049e96d5b1
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 00:47:12 +0200

    Created Room class
    Room - gameObject, the building blocks of the spaceship map

---
+++ Game/Rooms/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using InvadedGame.Engine.GameObject;
+
+namespace InvadedGame.Game.Rooms.Room
+{
+    public enum RoomState { Powered, Unpowered }
+
+    public class Room : GameObject
+    {
+        public RoomState State { get; set; } = RoomState.Powered;
+
+        public Room(string name):base(name) { }
+
+        public override string ToString() => $"{Name} ({State})";
+    }
+}
