17a5ed2f07e2df3dca9ab34e549f3ae55960145b
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 00:36:18 +0200

    Created GameWorld class
    Tempalte for a structure (scene) holding existing gameObjects. Responsible for updating gameObjects.

---
+++ Engine/GameWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Engine
+{
+    public abstract class GameWorld
+    {
+        public List<GameObject> Objects { get; } = new List<GameObject>();
+
+        public void AddObjects(GameObject obj) => Objects.Add(obj);
+
+        public void Update(float deltaTime)
+        {
+            foreach (var obj in Objects)
+                if (obj.Active) obj.Update(deltaTime);
+        }
+    }
+}
