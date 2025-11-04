ce0c29a093b18cb0f62d3f29ae27198c5a2d2936
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 01:12:15 +0200

    Created a Controller class
    Controller - either player or AI; datastructures responsible for controlling specific actors

---
+++ Game/Controllers/Controller.cs
@@ -0,0 +1,14 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Controllers
+{
+    public abstract class Controller
+    {
+        public abstract void PlanActions(Actors.Actor actor, GameWorld world);
+    }
+}
