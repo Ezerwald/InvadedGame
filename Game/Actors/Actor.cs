628ae508c4e54bf8f4846239af5d48553f6e0ecc
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-05 01:11:00 +0200

    Created an Actor abstract class
    Template for game actors. Actors are crew members, aliens etc, meaning units which can perform actions and interact with the spaceship.

---
+++ Game/Actors/Actor.cs
@@ -0,0 +1,23 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Controllers;
+using InvadedGame.Game.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Actors
+{
+    public abstract class Actor : GameObject
+    {
+        public Room CurrentRoom { get; internal set; }
+        public Controller Controller { get; set; }
+
+        protected Actor(string name, Room startingRoom)
+            : base(name)
+        {
+            CurrentRoom = startingRoom;
+        }
+    }
+}
