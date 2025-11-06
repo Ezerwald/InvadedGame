b09e05d428533ecedae15078d3b736ad19157d05
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-06 11:19:05 +0200

    Removed components field from gameObject
    No need in components

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -11,34 +11,12 @@ namespace InvadedGame.Engine
         public Guid Id { get; } = Guid.NewGuid();
         public string Name { get; }
         public bool Active { get; set; } = true;
-        private readonly List<Component> _components = new();
 
         protected GameObject(string name) 
         {
             Name = name;
         }
 
-        public T? GetComponent<T>() where T : Component =>
-            _components.OfType<T>().FirstOrDefault();
-
-        public void AddComponent(Component component)
-        {
-            component.Owner = this;
-            _components.Add(component);
-        }
-
-        public virtual void Update(float deltaTime)
-        {
-            foreach (var c in _components)
-                c.Update(deltaTime);
-        }
-
-        public override string ToString() => Name;
-    }
-
-    public abstract class Component
-    {
-        public GameObject Owner { get; internal set; }
-        public virtual void Update(float deltaTime) { }
+        public virtual void Update(GameWorld world, float deltaTime) { }
     }
 }
