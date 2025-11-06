28b48a9d4b0c76a39a950e6c6c801e112fdace3b
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-06 11:26:00 +0200

    Created Prorgram base with GameWorld

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InvadedGame.Engine;
+
+namespace StarshipGame
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var world = new GameWorld();
+        }
+    }
+}
