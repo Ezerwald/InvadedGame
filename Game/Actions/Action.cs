06e2d3f7455eef9b5817370733830fb859e948c1
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-08 18:43:14 +0200

    Created Action abstract class

---
+++ Game/Actions/Action.cs
@@ -0,0 +1,22 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Actions
+{
+    public abstract class Action
+    {
+        public string Name { get; }
+
+        protected Action(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Execute(GameWorld world, Actor actor);
+    }
+}
