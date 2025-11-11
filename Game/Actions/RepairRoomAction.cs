8b4fd1d50ab79db80b62563d509483187e8ca14e
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 01:28:20 +0200

    Created RepairRoomAction
    Repairs room in which actor currently is

---
+++ Game/Actions/RepairRoomAction.cs
@@ -0,0 +1,30 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Actors;
+using InvadedGame.Game.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Actions
+{
+    public class RepairRoomAction : GameAction
+    {
+        public RepairRoomAction() : base($"Break room action") { }
+
+        public override void Execute(GameWorld world, Actor actor)
+        {
+            Room room = actor.CurrentRoom;
+            if (room.IsOperational != true)
+            {
+                room.IsOperational = true;
+                Console.WriteLine($"Room {room.Name} was repaired");
+            }
+            else
+            {
+                Console.WriteLine("Room doesn't need repairing");
+            }
+        }
+    }
+}
