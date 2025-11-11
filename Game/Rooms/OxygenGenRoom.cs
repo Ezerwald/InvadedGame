e57bebf546429d373f9f43c554d8094f53498db8
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 00:43:02 +0200

    Created OxygenGenRoom
    Room responsible for generating oxygen on the spaceship

---
+++ Game/Rooms/OxygenGenRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Rooms
+{
+    public class OxygenGenRoom : Room
+    {
+        public OxygenGenRoom(string name) : base(name){}
+    }
+}
