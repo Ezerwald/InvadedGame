443f38d976b9f567a82d4d74eab3d2122b8d4c9b
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 00:33:31 +0200

    Created OxygenTracker
    Keeps track of oxygen on spaceship

---
+++ Game/ResourceTrackers/OxygenTracker.cs
@@ -0,0 +1,29 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.ResourceTrackers
+{
+    public class OxygenTracker : GameObject
+    {
+        public int OxygenValue { get; private set; } = 0;
+        public OxygenTracker(string name, int oxygenLevel)
+            : base(name)
+        {
+            OxygenValue = oxygenLevel;
+        }
+
+        public void SpendOxygen(int spentOxygenValue)
+        {
+            OxygenValue -= spentOxygenValue;
+        }
+
+        public void AddOxygen(int addedOxygenValue)
+        {
+            OxygenValue += addedOxygenValue;
+        }
+    }
+}
