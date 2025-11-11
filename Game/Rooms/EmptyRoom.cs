70966e58da4fff46092d96386aae5afb0974c1c0
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 00:33:11 +0200

    Created emptyRoom class
    If broken causes oxygen leakage

---
+++ Game/Rooms/EmptyRoom.cs
@@ -0,0 +1,15 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.ResourceTrackers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Rooms
+{
+    public class EmptyRoom : Room
+    {
+        public EmptyRoom(string name) : base(name) { }
+    }
+}
