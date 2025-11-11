c62e1b26945d879695357914d04e634515463b44
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 01:28:28 +0200

    Create CrewActor.cs
    Actor of crew

---
+++ Game/Actors/CrewActor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Actors
+{
+    public class CrewActor : Actor
+    {
+        public CrewActor(string name, Room startingRoom) : base(name, startingRoom) {}
+    }
+}
