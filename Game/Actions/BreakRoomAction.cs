1caa0b7119370ad2d4356863b8eb9c35dbd065cf
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 01:27:55 +0200

    Created BreakRoomAction
    Breaks room in which is current actor

---
+++ Game/Actions/BreakRoomAction.cs
@@ -0,0 +1,31 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Actors;
+using InvadedGame.Game.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Actions
+{
+    public class BreakRoomAction : GameAction
+    {
+        public BreakRoomAction() : base($"Break room action"){}
+
+        public override void Execute(GameWorld world, Actor actor)
+        {
+            Room room = actor.CurrentRoom;
+
+            if (room.IsOperational == true)
+            {
+                room.IsOperational = false;
+                Console.WriteLine($"Room {room.Name} was broken");
+            }
+            else
+            {
+                Console.WriteLine("Room is already broken");
+            }
+        }
+    }
+}
