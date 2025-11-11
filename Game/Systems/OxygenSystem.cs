69f5355ac008388f2e6d858eff3b782c5fccf95b
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 00:34:20 +0200

    Created OxygenSystem
    Object, responsible for keeping track of rooms affecting oxygen level in spaceship:
    - Broken empty rooms cause leakage

---
+++ Game/Systems/OxygenSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InvadedGame.Engine;
+using InvadedGame.Game.ResourceTrackers;
+using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Systems
+{
+    public class OxygenSystem : GameObject
+    {
+        public OxygenSystem(string name) : base(name ){}
+
+        public void Update(GameWorld world, float deltaTime)
+        {
+            IEnumerable<Room> rooms = world.FindObjectsOfType<Room>();
+            foreach (Room room in rooms)
+            {
+                if ((room is EmptyRoom) && (room.IsOperational == false))
+                {
+                    OxygenTracker? oxygen = world.FindObjectOfType<OxygenTracker>();
+                    if (oxygen != null)
+                    {
+                        oxygen.SpendOxygen(1);
+                    }
+                }
+            }
+        }
+    }
+}
