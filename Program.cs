83a78c78116ef4d050a6c9bb31c23d978b8e2b8e
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-12 01:30:01 +0200

    Wrote a first testing scenario
    ToDo:
    - Fix getConnectorTo(): move ConnectorsList from Room to separate helper

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using InvadedGame.Engine;
+using InvadedGame.Game.Actions;
+using InvadedGame.Game.Actors;
+using InvadedGame.Game.Rooms;
 
 namespace StarshipGame
 {
@@ -14,6 +16,35 @@ namespace StarshipGame
         private static void Main()
         {
             var world = new GameWorld();
+
+            Room room1 = new EmptyRoom("EmptyRoom 1");
+            Room room2 = new OxygenRoom("OxygenRoom 1");
+
+            world.AddObject(room1);
+            world.AddObject(room2);
+            world.AddObject(
+                new Connector(
+                    room1,
+                    room2,
+                    ConnectorType.Corridor,
+                    "Connector 1"
+                )
+            );
+
+            Actor actor1 = new CrewActor("Vlad", room1);
+
+            world.AddObject(actor1);
+
+            GameAction moveAction = new MoveAction(room2);
+            GameAction breakAction = new BreakRoomAction();
+            GameAction repairAction = new RepairRoomAction();
+
+            List<GameAction> actions = [moveAction, breakAction, repairAction];
+            foreach (GameAction action in actions)
+            {
+                action.Execute(world, actor1);
+            }
+
         }
     }
 }
