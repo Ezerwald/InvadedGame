c6352ae18e98469eebcea8349440da9283b2f790
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-17 17:52:00 +0200

    Created PhaseManager
    Manages current game phases.
    Phases:
    1. Planning phase - player decides actions for it's actors
    2. Execution phase - planned actions are executed in order
    3. End phase - resources are lost/produced etc

---
+++ Game/PhaseManager.cs
@@ -0,0 +1,43 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game
+{
+    public enum GamePhase
+    {
+        PlanningPhase,
+        ExecutionPhase,
+        EndPhase
+    }
+
+    public class PhaseManager : GameObject
+    {
+        public GamePhase CurrentPhase { get; private set; } = GamePhase.PlanningPhase;
+
+        public PhaseManager(string name):base(name) { }
+
+        public override void Update(GameWorld world, float deltaTime)
+        {
+            if (this.CurrentPhase == GamePhase.PlanningPhase)
+            {
+                Console.WriteLine("Planning phase happening");
+            }
+            switch (this.CurrentPhase)
+            {
+                case GamePhase.PlanningPhase:
+                    Console.WriteLine("Planning phase happening");
+                    break;
+                case GamePhase.ExecutionPhase:
+                    Console.WriteLine("Execution phase is happening");
+                    break;
+                case GamePhase.EndPhase:
+                    Console.WriteLine("End phase is happening");
+                    break;
+            }
+        }
+    }
+}
