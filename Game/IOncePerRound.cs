dc94b4ae0fb04226425716974f17797f5db9e72c
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-18 21:42:23 +0200

    Created IOncePerRound interface
    Interface for systems with functionality activating once per round at the End Phase

---
+++ Game/IOncePerRound.cs
@@ -0,0 +1,17 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game
+{
+    public interface IOncePerRound
+    {
+        void ExecuteRoundEffect(GameWorld world, float deltaTime);
+
+        event Action<IOncePerRound>? Completed;
+    }
+
+}
