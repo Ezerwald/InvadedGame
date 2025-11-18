37b555a6119c4fe6896852ba9f358c6f0bad5005
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-18 22:22:39 +0200

    Moved PhaseManager to GamePhases folder

---
+++ Game/Interfaces/IPhaseController.cs
@@ -0,0 +1,17 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Interfaces
+{
+    public interface IPhaseController
+    {
+        void OnEnter(GameWorld world);
+        void OnExit(GameWorld world);
+        void Update(GameWorld world, float deltaTime);
+    }
+
+}
