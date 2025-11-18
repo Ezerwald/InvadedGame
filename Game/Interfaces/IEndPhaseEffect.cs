77cd14a5763ca55c360b80bc96b68d7b8a78b61e
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-18 22:00:12 +0200

    Renamed IOncePerRound to IEndPhaseEffect

---
+++ Game/Interfaces/IEndPhaseEffect.cs
@@ -0,0 +1,17 @@
+﻿using InvadedGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Interfaces
+{
+    public interface IEndPhaseEffect
+    {
+        void ExecuteEndPhaseEffect(GameWorld world, float deltaTime);
+
+        event Action<IEndPhaseEffect>? Completed; 
+    }
+
+}
