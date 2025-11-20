4f3f4387c996ba545dd5ffe73c87469b73c83a7c
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-20 11:27:50 +0200

    Create ExecutionPhaseController.cs

---
+++ Game/GamePhases/ExecutionPhaseController.cs
@@ -0,0 +1,30 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.GamePhases
+{
+    public class ExecutionPhaseController : GameObject, IPhaseController
+    {
+        public ExecutionPhaseController(string name) : base(name) { }
+
+        public void OnEnter(GameWorld world, float deltaTime)
+        {
+            Console.WriteLine("Entering Execution Phase...");
+        }
+
+        public void OnExit(GameWorld world, float deltaTime)
+        {
+            Console.WriteLine("Exiting Execution Phase...");
+        }
+
+        public override void Update(GameWorld world, float deltaTime)
+        {
+            // Execution logic (movement, combat, etc.)
+        }
+    }
+}
