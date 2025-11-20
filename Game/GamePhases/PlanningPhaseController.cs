82b067cc11de94e128b8f5dfe7e2431774433b5a
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-20 11:27:52 +0200

    Create PlanningPhaseController.cs

---
+++ Game/GamePhases/PlanningPhaseController.cs
@@ -0,0 +1,31 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.GamePhases
+{
+    public class PlanningPhaseController : GameObject, IPhaseController
+    {
+        public PlanningPhaseController(string name) : base(name) { }
+
+        public void OnEnter(GameWorld world, float deltaTime)
+        {
+            Console.WriteLine("Entering Planning Phase...");
+        }
+
+        public void OnExit(GameWorld world, float deltaTime)
+        {
+            Console.WriteLine("Exiting Planning Phase...");
+        }
+
+        public override void Update(GameWorld world, float deltaTime)
+        {
+            // Planning logic (UI decisions, etc.)
+        }
+    }
+
+}
