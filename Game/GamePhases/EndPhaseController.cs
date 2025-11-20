c6c32922271ad9cf4f3f3151425fe34129f5508a
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-20 11:09:49 +0200

    GamePhases namespace updated

--- Game/GamePhases/EndPhaseController.cs
+++ Game/GamePhases/EndPhaseController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvadedGame.Game.PhaseManager
+namespace InvadedGame.Game.GamePhases
 {
     public class EndPhaseController : GameObject, IPhaseController
     {
--- Game/GamePhases/PhaseManager.cs
+++ Game/GamePhases/PhaseManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvadedGame.Game
+namespace InvadedGame.Game.GamePhases
 {
     public enum GamePhase
     {
