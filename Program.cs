7aae5f170f1a39807837d1a04ee2f317c8c3e655
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-21 14:44:57 +0200

    Updated Program.cs for testing new features
    Testing features:
    - Oxygen System
    - PhaseManager and Phase Controllers

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 using InvadedGame.Engine;
 using InvadedGame.Game.Actions;
 using InvadedGame.Game.Actors;
+using InvadedGame.Game.GamePhases;
+using InvadedGame.Game.Interfaces;
+using InvadedGame.Game.ResourceTrackers;
 using InvadedGame.Game.Rooms;
+using InvadedGame.Game.Systems;
 
 namespace StarshipGame
 {
@@ -18,33 +22,58 @@ namespace StarshipGame
             var world = new GameWorld();
 
             Room room1 = new EmptyRoom("EmptyRoom 1");
-            Room room2 = new OxygenRoom("OxygenRoom 1");
-
             world.AddObject(room1);
+
+            Room room2 = new OxygenRoom("OxygenRoom 1");
             world.AddObject(room2);
-            world.AddObject(
-                new Connector(
-                    room1,
-                    room2,
-                    ConnectorType.Corridor,
-                    "Connector 1"
-                )
-            );
+
+            Connector connector1_2 = new Connector(room1, room2, ConnectorType.Corridor, "Connector 1");
+            world.AddObject(connector1_2);
 
             Actor actor1 = new CrewActor("Vlad", room1);
-
             world.AddObject(actor1);
+
+
+            // Actions initialization
 
             GameAction moveAction = new MoveAction(room2);
             GameAction breakAction = new BreakRoomAction();
             GameAction repairAction = new RepairRoomAction();
 
-            List<GameAction> actions = [moveAction, breakAction, repairAction];
+            // Actions Execution 
+
+            List<GameAction> actions = [moveAction, breakAction];
             foreach (GameAction action in actions)
             {
                 action.Execute(world, actor1);
             }
 
+            // Oxygen System
+            OxygenTracker oxygenTracker = new OxygenTracker("Oxygen Tracker", 5);
+            world.AddObject(oxygenTracker);
+
+            OxygenSystem oxygenSystem = new OxygenSystem("Oxygen System", oxygenTracker);
+            world.AddObject(oxygenSystem);
+
+            // Phases initialization
+            PlanningPhaseController planningController = new PlanningPhaseController("Planning Phase Controller");
+            world.AddObject(planningController);
+
+            ExecutionPhaseController executionController = new ExecutionPhaseController("Execution Phase Controller");
+            world.AddObject(executionController);
+            
+            EndPhaseController endController = new EndPhaseController("End Phase Controller");
+            world.AddObject(endController);
+
+            PhaseManager phaseManager = new PhaseManager("Phase Manager", 
+                                                          planningController, 
+                                                          executionController, 
+                                                          endController);
+            world.AddObject(phaseManager);
+
+            // World Update (1 loop)
+            int deltaTime = 0;
+            world.Update(deltaTime);
         }
     }
 }
