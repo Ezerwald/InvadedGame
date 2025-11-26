4f689e87fe1ea7248f91b73296edeb0a39977ef9
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-11-26 21:10:08 +0200

    Created Logger
    Prints information into Console.
    Allows to categorize information: Debug, Info, Warning.
    Allows to filter information shown in Console.

---
+++ Game/Helpers/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace InvadedGame.Game.Helpers
+{
+    public static class Logger
+    {
+        private static bool logDebug = true;
+        private static bool logInfo = true;
+        private static bool logWarn = true;
+
+        public static void LogDebug(string message) { if (logDebug) { Console.WriteLine(message); } }
+
+        public static void LogInfo(string message) { if (logInfo) { Console.WriteLine(message); } }
+
+        public static void LogWarning(string message) { if (logWarn) { Console.WriteLine($"WARNING: {message}"); } }
+    }
+}
