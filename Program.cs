8d809e3382e9e34e2c28ba849f9b980c6ae0697e
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-02 22:51:02 +0200

    refactor: Change initialized objects in Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using InvadedGame.Game.Actions;
 using InvadedGame.Game.Actors;
 using InvadedGame.Game.GamePhases;
+using InvadedGame.Game.Helpers;
 using InvadedGame.Game.Rooms;
 using InvadedGame.Game.Systems;
 
@@ -13,56 +14,31 @@ namespace StarshipGame
         {
             var world = new GameWorld();
 
-            Room room1 = new EmptyRoom("EmptyRoom 1");
-            world.AddObject(room1);
+            // Initialize Rooms
+            Room emptyRoom1 = new EmptyRoom("'EmptyRoom 1'");
+            world.AddObject(emptyRoom1);
 
-            Room room2 = new EmptyRoom("EmptyRoom 2");
-            world.AddObject(room2);
+            Room emptyRoom2 = new EmptyRoom("'EmptyRoom 2'");
+            world.AddObject(emptyRoom2);
 
-            Connector connector1_2 = new Connector(room1, room2, ConnectorType.Corridor, "Connector 1");
-            world.AddObject(connector1_2);
+            // Initialize Connectors
+            world.AddObject(new Connector(emptyRoom1, emptyRoom2, ConnectorType.Corridor, "Connector 1"));
 
-            Actor actor1 = new CrewActor("Vlad", room1);
-            world.AddObject(actor1);
+            // Initialize Actors
+            world.AddObject(new CrewActor("Vlad", emptyRoom1));
+            world.AddObject(new CrewActor("Stefan", emptyRoom2));
 
+            // Initialize Oxygen System
+            world.AddObject(new OxygenSystem("Oxygen System", 5));
 
-            // Actions initialization
-
-            GameAction moveAction = new MoveAction(room2);
-            GameAction breakAction = new BreakRoomAction();
-            GameAction repairAction = new RepairRoomAction();
-
-            // Planning actions for actor
-            actor1.PlanAction(moveAction);
-            actor1.PlanAction(breakAction);
-
-            // Oxygen System
-            OxygenSystem oxygenSystem = new OxygenSystem("Oxygen System", 5);
-            world.AddObject(oxygenSystem);
-
-            // Phases initialization
-            PlanningPhaseController planningController = new PlanningPhaseController();
-
-            ExecutionPhaseController executionController = new ExecutionPhaseController();
-            
-            EndPhaseController endController = new EndPhaseController();
-
-            PhaseManager phaseManager = new PhaseManager(
-                "Phase Manager", 
-                world, 
-                planningController, 
-                executionController, 
-                endController
-            );
-            world.AddObject(phaseManager);
+            // Initialize Phases Manager
+            world.AddObject(new PhaseManager("Phase Manager", world));
 
             world.Start();
 
             while (world.Running)
             {
                 world.Update();
-
-                Console.Read();
             }
         }
     }
