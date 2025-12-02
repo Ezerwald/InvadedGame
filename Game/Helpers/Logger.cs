d259ce8e61f1faed6bbb3a519d0ad33520bdcd8d
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-02 22:45:40 +0200

    feat: Add EnableLogger option in Logger.cs to turn off all logging

--- Game/Helpers/Logger.cs
+++ Game/Helpers/Logger.cs
@@ -6,7 +6,8 @@ namespace InvadedGame.Game.Helpers
 {
     public static class Logger
     {
-        public static bool EnableDebug { get; set; } = true;
+        public static bool EnableLogger { get; set; } = true;
+        public static bool EnableDebug { get; set; } = false;
         public static bool EnableInfo { get; set; } = true;
         public static bool EnableWarn { get; set; } = true;
         public static bool EnableError { get; set; } = true;
@@ -19,29 +20,56 @@ namespace InvadedGame.Game.Helpers
 
         public static void Debug(string message, GameObject? obj = null)
         {
+            if (!EnableLogger)
+            {
+                return;
+            }
+
             if (EnableDebug)
                 Write("DEBUG", Format(message, obj));
         }
 
         public static void Info(string message, GameObject? obj = null)
         {
+            if (!EnableLogger)
+            {
+                return;
+            }
+
             if (EnableInfo)
                 Write("INFO", Format(message, obj));
         }
 
         public static void Warn(string message, GameObject? obj = null)
         {
+            if (!EnableLogger)
+            {
+                return;
+            }
+
             if (EnableWarn)
                 Write("WARN", Format(message, obj));
         }
 
         public static void Error(string message, GameObject? obj = null)
         {
+            if (!EnableLogger)
+            {
+                return;
+            }
+
             if (EnableError)
                 Write("ERROR", Format(message, obj));
         }
 
         private static string Format(string message, GameObject? obj) =>
             obj == null ? message : $"{obj.Name}: {message}";
+
+        public static void PauseAndClear(string notification = null)
+        {
+            Console.WriteLine($">> Press ENTER to continue <<");
+            Console.ReadLine();
+            Console.Clear();
+        }
     }
 }
