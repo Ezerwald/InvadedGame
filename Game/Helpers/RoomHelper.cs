dd1edd3bfbd134ea893c565d5b9c1ce0f8c1de0c
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-02 22:46:19 +0200

    feat: Introduced RoomHelper for retrieving connected rooms

---
+++ Game/Helpers/RoomHelper.cs
@@ -0,0 +1,27 @@
+﻿using InvadedGame.Engine;
+using InvadedGame.Game.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvadedGame.Game.Helpers
+{
+    public static class RoomHelper
+    {
+        public static List<Room> GetConnectedRooms(Room room, GameWorld world)
+        {
+            var connectors = world.FindObjectsOfType<Connector>();
+            var connected = new List<Room>();
+
+            foreach (var c in connectors)
+            {
+                if (c.RoomA == room) connected.Add(c.RoomB);
+                if (c.RoomB == room) connected.Add(c.RoomA);
+            }
+
+            return connected;
+        }
+    }
+}
