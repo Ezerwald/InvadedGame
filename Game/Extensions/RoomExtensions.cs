e8edb1338e0b11628c0fe6e8763ee6d9b99e1941
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-18 20:34:33 +0200

    feat: create RoomExtensions and Room.Deconstruct

---
+++ Game/Extensions/RoomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Extensions
+{
+    public static class RoomExtensions
+    {
+        public static void Deconstruct(
+            this Room room,
+            out string name,
+            out bool isOperational)
+        {
+            name = room.Name;
+            isOperational = room.IsOperational;
+        }
+    }
+}
