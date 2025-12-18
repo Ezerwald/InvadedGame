c61b8cf11914002dcf31ab04faf16cce5c253349
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-18 20:20:58 +0200

    feat: Create MovementException

---
+++ Game/Exceptions/MovementException.cs
@@ -0,0 +1,12 @@
+﻿using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Exceptions
+{
+    public class MovementException : Exception
+    {
+        public MovementException(string message) : base(message) { }
+
+        public MovementException(Room from, Room to, string reason)
+            : base($"Cannot move from {from.Name} to {to.Name}: {reason}") { }
+    }
+}
