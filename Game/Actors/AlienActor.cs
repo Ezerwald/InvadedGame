4f5c004feeccaad09490b747d874aef745f1654f
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-12-18 17:45:35 +0200

    feat: Create AlienActor class

---
+++ Game/Actors/AlienActor.cs
@@ -0,0 +1,9 @@
+﻿using InvadedGame.Game.Rooms;
+
+namespace InvadedGame.Game.Actors
+{
+    public class AlienActor : Actor
+    {
+        public AlienActor(string name, Room startingRoom) : base(name, startingRoom) { }
+    }
+}
